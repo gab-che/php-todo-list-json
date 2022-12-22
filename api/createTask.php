3fc1eceb1c9324d458ecd762a2dcbc55f155acdd
gab-che <gabriellachessa1@gmail.com>
2022-12-22 16:45:34 +0100

    php adds task through $_POST in json file

--- api/createTask.php
+++ api/createTask.php
@@ -1,14 +1,15 @@
 <?php
+array_key_exists('new_task', $_POST) ? $_POST['new_task'] : $_POST['new_task'] = '';
+
 $tasks = json_decode(file_get_contents("../tasks.json"), true);
 
 $new_task = [
-    'task_name' => "Hitchhike across space",
-    'task_status' => true,
+    'task_name' => $_POST['new_task'],
+    'task_status' => false,
 ];
 
 $tasks[] = $new_task;
 
-$new_task_json = json_encode($tasks, JSON_PRETTY_PRINT);
-file_put_contents("../tasks.json", $new_task_json);
+file_put_contents("../tasks.json", json_encode($tasks, JSON_PRETTY_PRINT));
 
 var_dump($tasks);
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-array_key_exists('new_task', $_POST) ? $_POST['new_task'] : $_POST['new_task'] = '';
 ?>
 
 <!DOCTYPE html>
